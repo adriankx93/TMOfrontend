b9a1443d047d8378c5233165b2d18a38bf4ba139
adriankx93 <kicioradrian@gmail.com>
2025-07-03 20:34:39 +0200

    EditUserPage.jsx

---
+++ src/pages/EditUserPage.jsx
@@ -0,0 +1,9 @@
+import UserForm from "../components/Admin/UserForm";
+
+export default function EditUserPage() {
+  return (
+    <div className="max-w-lg mx-auto mt-8">
+      <UserForm />
+    </div>
+  );
+}
