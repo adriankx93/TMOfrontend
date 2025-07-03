f2b2cd6169b24222517c953e9c78dc1331d67a30
adriankx93 <kicioradrian@gmail.com>
2025-07-03 20:35:08 +0200

    ProfilePage.jsx

---
+++ src/pages/ProfilePage.jsx
@@ -0,0 +1,9 @@
+import ProfileForm from "../components/Profile/ProfileForm";
+
+export default function ProfilePage() {
+  return (
+    <div className="max-w-lg mx-auto mt-8">
+      <ProfileForm />
+    </div>
+  );
+}
