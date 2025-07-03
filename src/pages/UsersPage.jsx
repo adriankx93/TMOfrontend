879c14733203d35ccb29377db6bb785796cbe860
adriankx93 <kicioradrian@gmail.com>
2025-07-03 20:34:13 +0200

    UsersPage.jsx

---
+++ src/pages/UsersPage.jsx
@@ -0,0 +1,9 @@
+import UserList from "../components/Admin/UserList";
+
+export default function UsersPage() {
+  return (
+    <div className="max-w-5xl mx-auto">
+      <UserList />
+    </div>
+  );
+}
