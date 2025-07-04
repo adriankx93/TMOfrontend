25dbbc742a7c81054de9429408a5c38590808ed8
adriankx93 <kicioradrian@gmail.com>
2025-07-04 09:06:00 +0200

    Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="bg-blue-900 text-white text-center py-6 mt-16 rounded-t-2xl shadow-2xl">
-      &copy; {new Date().getFullYear()} SINGU. All rights reserved.
+      &copy; {new Date().getFullYear()} TMO. Wszystkie prawa zastrzeżone.
     </footer>
   );
 }
