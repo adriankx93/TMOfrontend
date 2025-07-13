5839e35cb70420cbaf43ac2b49e7de7144495fb0
adriankx93 <kicioradrian@gmail.com>
2025-07-13 15:51:09 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,19 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        shake: {
+          '10%, 90%': { transform: 'translateX(-2px)' },
+          '20%, 80%': { transform: 'translateX(4px)' },
+          '30%, 50%, 70%': { transform: 'translateX(-8px)' },
+          '40%, 60%': { transform: 'translateX(8px)' },
+        },
+      },
+      animation: {
+        shake: 'shake 0.4s cubic-bezier(.36,.07,.19,.97) both'
+      },
+    },
   },
   plugins: [],
 }
