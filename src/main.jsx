d0a881bb971a569f27396c665e145814e9a10b35
adriankx93 <kicioradrian@gmail.com>
2025-07-14 20:06:20 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,12 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
-import AuthGuard from "./components/AuthGuard"; // <-- dodaj import
+// import AuthGuard from "./components/AuthGuard"; // tymczasowo wyłącz
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <AuthGuard>
-        <App />
-      </AuthGuard>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
